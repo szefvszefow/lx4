50b03e1e901855beffb00f061281b37223b3f40b
szefvszefow <99558437+szefvszefow@users.noreply.github.com>
2022-02-12 21:10:16 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+  <form action="apicreatelink.php" method="GET">
+  <title>Stworz link!</title>
+    </head>
+
+<body>
+np. https://github.com <br>
+<input type="text" name="url" placeholder="URL WWW z https/http"> <br>
+np. teamX <br>
+<input type="text" name="nazwa" placeholder="NAZWA"> <br>
+<input type="submit" class="button" value="send">
+<h1>masz blad? pisz na discordzie: szef szefow#5971</h1>
+github: za nie dlugo!
+</form>
+
+
+    </body>
+</html>
